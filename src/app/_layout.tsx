8fa96894b24108b760151b8dc9add0f61be641d3
Khalid Deniev <haliddeniev88@gmail.com>
2025-01-23 11:34:41 +0300

    install deps

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,10 @@
-import OpenAI from "openai";
 import { Text } from "react-native";
-import axios from "axios";
-
-const config = {
-  onDownloadProgress: (progressEvent) => {
-    console.log(progressEvent.total);
-  },
-};
-
-axios.get("https://jsonplaceholder.typicode.com/todos", config);
+import { SafeArea } from "@/src/components/ui";
 
 export default function RootLayout() {
-  return <Text>12312</Text>;
+  return (
+    <SafeArea>
+      <Text>23</Text>
+    </SafeArea>
+  );
 }
